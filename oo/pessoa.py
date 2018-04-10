0d98fbe362ffab764617c54769d959ef872de062
brunofalis0@gmail.com <cindylola24#>
2018-04-09 23:34:03 -0300

    creating greet

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,9 @@
-class Pessoa:
-    pass
+class Person():
+    def greet(self):
+        return f'Hello {id(self)}'
+
+if __name__ == '__main__':
+    p = Person()
+    print(Person.greet(p))
+    print(id(p))
+    print(p.greet())
