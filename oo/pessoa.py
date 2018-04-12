73669017ef5e29910627861b44005415d3dab157
brunofalis0@gmail.com <cindylola24#>
2018-04-11 23:26:27 -0300

    created atributes of instace, age and name

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,16 @@
 class Person():
+    def __init__(self, name=None, age=0):
+        self.age = age
+        self.name = name
+
     def greet(self):
         return f'Hello {id(self)}'
 
 if __name__ == '__main__':
-    p = Person()
+    p = Person('Bruno')
     print(Person.greet(p))
     print(id(p))
     print(p.greet())
+    print(p.name)
+    p.name = 'Bruno Falis'
+    print(p.name)
